a71b2c80c5cd9bdeb6a94cd353bd6a8a4565fa45
ohtubo <brte14020@gmail.com>
2021-10-18 16:27:24 +0000

    [Add]_teamLab_question8,9

--- teamLab/question8.rb
+++ teamLab/question8.rb
@@ -3,3 +3,24 @@
 # * 十進法で書いた場合に「3」が含まれる。
 # 例えば、9、31、42、135は何れも「A的な数」です。
 # 1から20000までの整数のうち「A的な数」だけを足し合わせたときの合計を求めてください。 
+
+num = 20000
+sum = 0
+
+for i in 1..num
+  if i % 3 == 0
+    sum += i
+    print "[倍]#{i}, "
+  else
+    str = i.to_s
+    for ii in 1..str.length
+      if str[ii] == "3"
+        sum += i
+        print "[A]#{i}, "
+        break
+      end
+    end
+  end
+end
+
+puts "合計:#{sum}"
