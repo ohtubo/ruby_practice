8f24cf88dd2278b1d61537c4ebf81350f4a2d116
ohtubo <brte14020@gmail.com>
2021-10-21 14:49:39 +0000

    [Add][Software_Design_plus]_chapter4_3

--- Software_Design_plus/sample4_2.rb
+++ Software_Design_plus/sample4_2.rb
@@ -44,3 +44,73 @@ puts charge(3)
 puts charge(12)
 puts charge(16)
 puts charge(25)
+
+puts '------------4.7配列詳しく-----------------------------------------'
+
+a = [1, 2, 3, 4, 5]
+puts a[1,3]
+puts a.values_at(0,2,4)
+
+puts a.last
+puts a.last(2)
+
+puts '------------4.7配列[集合]-----------------------------------------'
+
+a = [1,2,3]
+b = [3,4,5]
+
+print "a|b = #{a|b}, "
+print "a - b = #{a - b}, "
+puts "a & b = #{a & b}"
+
+puts '------------4.7メソッドの可変長引数-----------------------------------------'
+
+def greeting(*names)
+  "#{names.join('と')}、こんにちは！"
+end
+
+puts greeting('田中さん')
+puts greeting('田中さん', '鈴木さん')
+puts greeting('田中さん', '鈴木さん', '佐藤さん' )
+
+puts '------------4.7配列を簡素に作る-----------------------------------------'
+
+puts %w!appel melon orange!
+puts %w(appel melon orange painappule) 
+
+puts %w(my\appel my\ melon my\ orange my\ painappule) 
+
+puts '------------4.7文字列を配列化-----------------------------------------'
+
+puts 'Ruby'.chars
+puts 'Ruby,PHP,perl,java'.split(',')
+
+puts '------------4.7配列に初期値を設定する-----------------------------------------'
+
+puts a = Array.new
+puts a = Array.new(5)
+
+puts a = Array.new(10){ |n| n % 3 + 1}
+
+puts '------------4.7配列に初期値を設定する注意点-----------------------------------------'
+
+a = Array.new(5, 'defalt')
+
+str = a[0]
+
+str.upcase!
+
+puts a
+puts '全ての要素が紐づいている'
+
+
+a = Array.new(5){ 'defalt' }
+
+str = a[0]
+
+str.upcase!
+
+puts a
+puts 'ブロックを使うと紐づけされない'
+
+
---
+++ Software_Design_plus/sample4_3.rb
@@ -0,0 +1,42 @@
+puts '------------4.8ブロック詳細-----------------------------------------'
+
+fruist = ['apple', 'orange', 'melon']
+
+#each_with_indexで[i]に要素番号が入る
+fruist.each_with_index { |fruit, i| puts "#{i}: #{fruit}"}
+
+#mapメソッドの場合 with_indexに値を渡すとその数から開始される
+fruist.map.with_index(1) { |fruit, i| puts "#{i}: #{fruit}"}
+
+#`a`を含むかつ、奇数の要素番号は削除
+puts fruist.delete_if.with_index { |fruit, i| fruit.include?('a') && i.odd? }
+
+puts '------------4.8配列がブロック引数に渡される-----------------------------------------'
+
+dimesion = [
+  [10, 20],
+  [30, 40],
+  [50, 60],
+  ]
+  
+areas = []
+
+dimesion.each do |length, width|
+  areas << length * width
+end
+
+puts areas
+
+# each_with_indexの場合 (length, width)にしないとlengthにまとめてはいる
+
+dimesion.each_with_index do |(length, width), i|
+  puts "length：#{length}, width：#{width}, i：#{i}"
+end
+
+
+File.open("./sample.txt", "w")do |file|
+  file.puts("1行目のテキストです")
+  file.puts("2行目のテキストです")
+  file.puts("3行目のテキストです")
+end
+
