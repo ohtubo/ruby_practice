2f75e9dacac9ad8024f540f1498bd174d4caf23a
ohtubo <brte14020@gmail.com>
2021-11-03 02:12:30 +0000

    [Add][Software_Design_plus]_chapter6_1[3]

--- Software_Design_plus/sample6_1.rb
+++ Software_Design_plus/sample6_1.rb
@@ -37,6 +37,77 @@ puts 'hello' !~ /\d{3}-\d{4}/
 
 text = '私の誕生日は199年7月17日です'
 m = /(\d+)年(\d+)月(\d+)日/.match(text)
+print m
+puts
 puts m[1]
 puts m[2]
 puts m[3]
+
+#キャプチャに名前を付ける
+text = '私の誕生日は1996年8月08日です'
+m = /(?<year>\d+)年(?<month>\d+)月(?<day>\d+)/.match(text)
+
+puts m[:year]
+puts m[:month]
+puts m[:day]
+
+puts m['year']
+
+puts m[2]
+
+text = '私の誕生日は1995年3月08日です'
+
+if /(?<year>\d+)年(?<month>\d+)月(?<day>\d+)/ =~ text
+  puts "#{year}/#{month}/#{day}"
+end
+
+#逆記入は不可能
+if  text =~ /(?<year>\d+)年(?<month>\d+)月(?<day>\d+)/ 
+  puts "#{year}/#{month}/#{day}"
+end
+
+puts '-----------------6.3.正規表現と組み合わせると便利なStringメソッド------------------------------------'
+
+puts '-----------------6.3.scan------------------------------------'
+
+print '123 456 789'.scan(/\d+/)
+puts
+print '1999年01月01日 2020年12月31日'.scan(/(\d+)年(\d+)月(\d+)日/)
+puts
+print '1999年01月01日 2020年12月31日'.scan(/(?:\d+)年(?:\d+)月(?:\d+)日/)
+puts
+
+puts '-----------------6.3.[],slice------------------------------------'
+
+text = '郵便番号は123-4567です'
+print text[/\d{3}-\d{4}/]
+puts
+
+text = '郵便番号は123-4567 456-6789です'
+print text[/\d{3}-\d{4}/]
+puts
+
+text = '誕生日は2021年01月10日です'
+print text[/(\d+)年(\d+)月(\d+)日/]
+puts
+print text[/(\d+)年(\d+)月(\d+)日/, 3]
+puts
+
+#シンボルでキャプチャの名前を表示する
+print text[/(?<year>\d+)年(?<month>\d+)月(?<day>\d+)/, :day]
+puts
+
+#文字列でキャプチャの名前を表示する
+print text[/(?<year>\d+)年(?<month>\d+)月(?<day>\d+)/, 'day']
+puts
+
+text = '郵便番号は123-4567です'
+print text.slice(/\d{3}-\d{4}/)
+puts
+
+text = '郵便番号は123-4567です'
+print text.slice!(/\d{3}-\d{4}/)
+puts text
+puts '-----------------6.3.spliit------------------------------------'
+
+puts '-----------------6.3.gsub,gsub!------------------------------------'
