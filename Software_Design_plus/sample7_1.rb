40e5f408959312fe213118db040cc9293d7415c9
ohtubo <brte14020@gmail.com>
2021-11-06 23:36:02 +0000

    [Add][Software_Design_plus]_chapter7_1[2]

--- Software_Design_plus/sample7_1.rb
+++ Software_Design_plus/sample7_1.rb
@@ -38,6 +38,24 @@ end
 # users[0].country = 'japan'
 # users[0].first_mame = 'Carol'
 
+puts '-----------------7.2.属性------------------------------------'
+
+class User2
+
+  attr_accessor :first_name
+
+  def initialize(first_name,last_name,age)
+    @first_name = first_name
+    @last_name = last_name
+    @age = age
+  end
+
+end
+
+user = User2.new('Alice', 'Ruby', 20)
+puts user.first_name
+user.first_name = 'ありす'
+puts user.first_name
 
 
 
