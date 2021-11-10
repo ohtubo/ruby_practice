6843c743753deb34b8c40073938e1f5e665597ab
ohtubo <brte14020@gmail.com>
2021-11-10 23:21:46 +0000

    [Add][Software_Design_plus]_chapter7_[gate][3]

--- Software_Design_plus/ruby-book/lib/gate.rb
+++ Software_Design_plus/ruby-book/lib/gate.rb
@@ -1,17 +1,27 @@
-#改札判定プログラム
-
+# 改札機を表すクラス
 class Gate
-  STATIONS = [:umeda, :juso, :mikuni]
-  FARES = [150, 190]
+  STATIONS = [:umeda, :juso, :mikuni]# :nodoc:
+  FARES = [150, 190]# :nodoc:
   
+  # Gateオブジェクトの作成
+  # ====　引数
+  # * +name+ - 駅名
   def initialize(name)
     @name = name
   end
   
+  # 改札機を通って駅に入場する
+  # ====　引数
+  # * +ticket+ - 切符  
   def enter(ticket)
     ticket.stamp(@name)
   end
-  
+
+  # 改札機を通って駅に入場する
+  # ==== 引数
+  # * +ticket+ - 切符  
+  # ==== 戻り値
+  # * +boolean+ - 運賃が足りていれば+ture+、不足していれば+false+
   def exit(ticket)
     fare = calc_fare(ticket)
     fare <= ticket.fare
