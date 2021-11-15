0a346802e0520942c1bcf353e07465924ee32f6c
ohtubo <brte14020@gmail.com>
2021-11-15 13:55:06 +0000

    [Add][Software_Design_plus]_chapter7_4[4]

--- Software_Design_plus/sample7_4.rb
+++ Software_Design_plus/sample7_4.rb
@@ -59,3 +59,28 @@ puts '-----------------7.6.4.他class継承class作る--------------------------
   puts dvd.running_time
   
   puts '-----------------7.6.6.メソッドのオーバライド------------------------------------'
+  
+  class Product
+    attr_reader :name, :price
+    
+    def initialize(name, price)
+      @name = name
+      @price = price
+    end
+  end
+  
+  class DVD < Product
+    attr_reader :running_time
+    
+    def initialize(name,price,running_time)
+      super(name, price)
+      @running_time = running_time
+    end
+    
+  end
+  
+  product = Product.new('A great movie', 1000)
+  puts product.to_s
+  
+  dvd = DVD.new('A great movie', 1000, 120)
+  puts dvd.to_s
