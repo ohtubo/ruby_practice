d33c88c67ab70d64e406f627c012f587286a3214
ohtubo <brte14020@gmail.com>
2021-11-28 02:34:20 +0000

    [Add][Software_Design_plus]_chapter8_[deep_freezable][2]

--- Software_Design_plus/ruby-book/test/deep_freezable_test.rb
+++ Software_Design_plus/ruby-book/test/deep_freezable_test.rb
@@ -5,8 +5,23 @@ require './lib/team'
 
 class DeepFreezableTest < Minitest::Test
   
-  def test_deep_freeze
-    assert DeepFreezable
+  def test_deep_freeze_to_array
+    assert_equal ['Japan','US','India'], Team::COUNTRIES
+    
+    assert Team::COUNTRIES.frozen?
+    
+    assert Team::COUNTRIES.all?{ |country| country.frozen? }
+    
+  end
+  
+  def test_deep_freeze_to_hash
+    assert_equal(
+      { 'Japan' => 'yen', 'US' => 'dollar', 'India' => 'rupee'},
+      Bank::CURRENIES
+    )
+    
+    assert Bank::CURRENIES.frozen?
+    assert Bank::CURRENIES.all?{ |key, value| key.frozen? && value.frozen?}
   end
   
 end
